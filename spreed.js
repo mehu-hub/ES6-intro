31668de90bd9ba15a44cdfa7af6d37f7bba6bb89
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-02 21:09:29 +0600

    Spreed Operator

---
+++ spreed.js
@@ -0,0 +1,11 @@
+const max = Math.max(12, 85, 999, 78);
+// console.log(max)
+
+const numbers = [12, 84, 50, 999];
+
+const largest = Math.max(...numbers);
+// console.log(...numbers)
+// console.log(largest)
+
+const number2 = [111, 23, 33, ...numbers, 132, 56];
+// console.log(number2)
